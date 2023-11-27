fcc0562fa05b6207ebcfae6f1ffe937a98ec6ea9
ChrisTR1021 <christianridgell1021@gmail.com>
2023-11-27 01:05:42 -0500

    started javascript have url and api key but getting invalid API key

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 var searchButton = document.querySelector("#searchBttn");
-var inputCity = document.querySelector("#city-input");
-
+var inputCity = document.querySelector("#city-name");
+var apiKey = "6cb51ee488390e9ab28eca71544422ef";
 
 var cityLocationCoordinates = () => {
     // user can enter in designated city and extra space will be removed 
     var cityTitle = inputCity.value.trim(); 
     // will return if the cityTitle is empty
     if(!cityTitle) return;
-    console.log(cityTitle);
+    var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityTitle}&limit=1&appid=${APIKEY}';
+
+
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        console.log(data)
+    }).catch(() => {
+        alert("error fetching the coordinates");
+    });
+    
 }
 searchButton.addEventListener("click", cityLocationCoordinates);
